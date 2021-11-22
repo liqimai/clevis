709246b981dd13bb795f85c91f9370b664635474
liqimai <liqimai@outlook.com>
2021-11-22 15:24:21 +0800

    clevis program with HtmlRenderer and CliCommander

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io;
+
+use clevis::app::App;
+use clevis::commander::CliCommander;
+use clevis::log::DummyLogger;
+use clevis::render::HtmlRenderer;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    if cfg!(target_os = "macos") {
+        println!("I'm running on a macos machine!");
+    }
+
+    let mut app = App::new(DummyLogger, HtmlRenderer::new("screen.html", true).unwrap());
+    let commander = CliCommander::new(io::BufReader::new(io::stdin()), io::stdout());
+
+    app.run(commander);
+
+    Ok(())
 }
